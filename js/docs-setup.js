926f9eb3e55e164db06fe8c5f6319f4eb7f13e4e
Will Jones <the8bitsquid@gmail.com>
2015-12-18 17:15:38 -0600

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,96 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "Quick Start",
+      "keywords": "alert alert-info alert-warning angular api application automation bootstrap bower check class command commands computer config create dependencies development directory download explains file frameworks front-end getting-started getting-started_install-bower getting-started_install-grunt globally grunt grunt-cli guide haven install installed installs instructions interface io jquery js json listed manage manager node npm npmjs option org overview package peer platform plugins project quick read requires root start started third-party tools unfamiliar web"
+    },
+    {
+      "section": "api",
+      "id": "news",
+      "shortName": "news",
+      "type": "overview",
+      "moduleName": "news",
+      "shortDescription": "News and Events web app",
+      "keywords": "angular angular-carousel api app bootstrap duscroll events filter lib news nganimate ngresource ngroute ngsanitize overview route ua ualib ui web"
+    },
+    {
+      "section": "api",
+      "id": "news.Controller:newsListCtrl",
+      "shortName": "newsListCtrl",
+      "type": "controller",
+      "moduleName": "news",
+      "shortDescription": "Controller for the News and Exhibits web app route $scope",
+      "keywords": "$location $scope api app bind class controller exhibits filters function items label label-danger list news newsfilter newsfilters object params paramstoscope processfilters query return route service ualibnewsfactory uri variable web"
+    },
+    {
+      "section": "api",
+      "id": "news.controller:NewsTodayCtrl",
+      "shortName": "NewsTodayCtrl",
+      "type": "controller",
+      "moduleName": "news",
+      "shortDescription": "Convenience controller to be used with the newsCard directive, to display",
+      "keywords": "$filter $scope api controller convenience current directive display event events html item news news-card news-type newscard newstodayctrl ng-contorller ng-repeat ualibnewsfactory"
+    },
+    {
+      "section": "api",
+      "id": "news.news-item",
+      "shortName": "news.news-item",
+      "type": "interface",
+      "moduleName": "news",
+      "shortDescription": "Route module to display individual news items",
+      "keywords": "api display individual interface items module news news-item route"
+    },
+    {
+      "section": "api",
+      "id": "news.news-item.controller:newsItemCtrl",
+      "shortName": "newsItemCtrl",
+      "type": "controller",
+      "moduleName": "news.news-item",
+      "shortDescription": "Route controller when viewing individual news items",
+      "keywords": "$document $routeparams $scope api controller individual items news news-item route ualibnewsfactory viewing"
+    },
+    {
+      "section": "api",
+      "id": "news.news-item.directive:newsCard",
+      "shortName": "newsCard",
+      "type": "directive",
+      "moduleName": "news.news-item",
+      "shortDescription": "Directive to render different types of news items in a condensed list. This is useful for lists of the most current items and not intended for",
+      "keywords": "api archive condensed current directive event html intended item items json list lists load news news-card news-item news-type newscard ng-repeat object render rendering supported template templates tpl type types"
+    },
+    {
+      "section": "api",
+      "id": "news.news-item.newsItem",
+      "shortName": "news.news-item.newsItem",
+      "type": "service",
+      "moduleName": "news.news-item",
+      "shortDescription": "A resolve service for news-item route, which is injected into the news.controller:newsItemCtrl",
+      "keywords": "api controller injected news news-item newsitem resolve route service ualibnewsfactory"
+    },
+    {
+      "section": "api",
+      "id": "news.ualibNewsFactory",
+      "shortName": "news.ualibNewsFactory",
+      "type": "service",
+      "moduleName": "news",
+      "keywords": "$digest $filters $http $promise $resource $sce $scope allows angular angularjs api appendtransform archive array cached calendar call class config console custom cycle data default defaults docs events extend function functions headers https identifier identifiers initial items json label label-warning list log method modifications news object org overriding-the-default-transformations-per-request params performing refreshed response responsetransform rest retrieves returns send service status straight time today transform transformresponse ua ualibnewsfactory upcoming"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
